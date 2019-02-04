5eb5330634d6c2558c24c9fe7296841837423106
hynra <hynra@yahoo.com>
2019-02-04 14:23:57 +0700

    add apollo server to koa

--- app.js
+++ app.js
@@ -9,12 +9,34 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+const { ApolloServer, gql } = require('apollo-server-koa')
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+app.graphqlPath = server.graphqlPath
+
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
